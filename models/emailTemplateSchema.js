0ac14cbbc4ca4142455bb6b58b8067a5130249ae
Saadkhalid913 <Saadkhalid913@gmail.com>
2021-07-05 18:10:49 -0400

    removed uneeded files

--- models/emailTemplateSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const emailTemplateSchema = new mongoose.Schema({
-    name: String,
-    subject: String,
-    body: String
-})
-
-module.exports = emailTemplateSchema;
--- models/models.js
+++ models/models.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 const userSchema = require("./userSchema")
-const emailTemplateSchema = require("./emailTemplateSchema")
-const emailListSchema = require("./emailListSchema")
-const sentEmailSchema = require("./sentEmailSchema")
-
 
 const userModel = mongoose.model("users", userSchema);
-const sentEmailModel = mongoose.model("sentEmails", sentEmailSchema)
-const emailTemplateModel = mongoose.model("emailTemplates", emailTemplateSchema)
-const emailListModel = mongoose.model("emailLists", emailListSchema)
 
-module.exports = { userModel , sentEmailModel, emailListModel, emailTemplateModel } 
+
+module.exports = { userModel } 
